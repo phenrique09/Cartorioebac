d3edce67ccd9641a4f95b2f3e0e8f89114b170b4
Pablo Henrique <133898818+phenrique09@users.noreply.github.com>
2023-05-19 17:14:33 -0300

    Validação por senha
    Incluso no sistema a validação por senha do admin.

--- cartorio_ebac.c
+++ cartorio_ebac.c
@@ -109,52 +109,70 @@ int main() //funçao
 {
 	int opcao=0;  //definindo as variaveis
 	int x=1;  //enquanto o valor for 1, vai estar em funcionamento
+	char senhadigitada[10]="a"; //string
+	int comparacao;
 	
-	for (x=1;x=1;) //repetindo a funçao
-    {
-	   system("cls");  //sistema limpa a tela
+	printf("### Cartório da EBAC ###\n\n"); //inicio do menu
+	printf("Login De Administrador\n\n Digite a sua senha:"); //Login de Administrador
+	scanf("%s", senhadigitada);  //salva dentro de uma string
+	
+	comparacao = strcmp(senhadigitada, "admin"); //comparando se a senha é admin
+	
+	if (comparacao == 0) //se a senha for admin
+	{
+		system("cls"); //pausa a tela
+		for (x=1;x=1;) //repetindo a funçao
+        {
+	       system("cls");  //sistema limpa a tela
 	   
-	   setlocale(LC_ALL, "Portuguese"); //definindo a linguagem
+	       setlocale(LC_ALL, "Portuguese"); //definindo a linguagem
 	
-	   printf("### Cartório da EBAC ###\n\n"); //inicio do menu
-	   printf("Escolha a opção desejada do menu:\n\n"); //comentario
-	   printf("\t1 - Registrar nomes\n"); //opçao 1
-	   printf("\t2 - Consultar nomes\n"); //opçao 2
-	   printf("\t3 - Deletar nomes\n\n"); //opçao 3
-	   printf("\t4 - Sair do sistema\n\n"); //opçao 4
-	   printf("Opçao: "); //fim do menu
+	       printf("### Cartório da EBAC ###\n\n"); //inicio do menu
+    	   printf("Escolha a opção desejada do menu:\n\n"); //comentario
+	       printf("\t1 - Registrar nomes\n"); //opçao 1
+    	   printf("\t2 - Consultar nomes\n"); //opçao 2
+    	   printf("\t3 - Deletar nomes\n\n"); //opçao 3
+    	   printf("\t4 - Sair do sistema\n\n"); //opçao 4
+    	   printf("Opçao: "); //fim do menu
 	
-	   scanf("%d", &opcao); //armazenando a escolha do usuario
+     	   scanf("%d", &opcao); //armazenando a escolha do usuario
 	
-	   system("cls");  //sistema limpa a tela
+	       system("cls");  //sistema limpa a tela
 	   
-	   switch(opcao) //testa igualdades
-	   {
-	   	    case 1: //caso for 1
-	   	    registro();
-	   		break; //fecha chave
+    	   switch(opcao) //testa igualdades
+    	   {
+	   	        case 1: //caso for 1
+	   	        registro();
+	   	    	break; //fecha chave
 	   	    
-	   	    case 2: //caso for 2
-	   		consultar();
-	   	    break; //fecha chave
+	   	        case 2: //caso for 2
+	   	      	consultar();
+	   	        break; //fecha chave
 	   	    
-	   	    case 3: //caso for 3
-	   		deletar();
-	   	    break; //fecha chave
+	   	        case 3: //caso for 3
+	   	    	deletar();
+	   	        break; //fecha chave
 	   	    
-	   	    case 4: //caso for 4
-	   	    printf("Obrigado por utilizar o sistema!\n"); //comentario
-	   	    return 0; //retorna ao valor 0
-	   	    break; //fecha chave
+	   	        case 4: //caso for 4
+	   	        printf("Obrigado por utilizar o sistema!\n"); //comentario
+	   	        return 0; //retorna ao valor 0
+	   	        break; //fecha chave
 	   	    
 	   	    
-	   	    default:   //se nao for nenhuma opçao
-	   	    printf("Essa opção não está disponivel!\n"); //comentario
-		    system("pause"); //sistema pausa a tela
-	   		break;  //fecha chave
+	   	        default:   //se nao for nenhuma opçao
+	   	        printf("Essa opção não está disponivel!\n"); //comentario
+		        system("pause"); //sistema pausa a tela
+	   	    	break;  //fecha chave
 	   	
 	   	
-	   }
+	       }
 	
-    }     
+        } 
+		
+	}
+    
+	else
+			printf("Senha incorreta!"); 
+    
 }
+    
